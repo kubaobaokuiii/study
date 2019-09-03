8837415359b3b14b54c6bab9cb3347bcd88cbd15
guoshuang <18232932923@163.com>
2019-09-03 10:46:01 +0800

    Initial commit

---
+++ src/com/compay/dateStudy.java
@@ -0,0 +1,32 @@
+package com.compay;
+
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
+public class dateStudy {
+
+    public static void main(String[] args){
+
+//        Calendar calendar = Calendar.getInstance();
+//
+//        calendar.add(Calendar.DATE,1);
+//
+//        System.out.println(calendar.getTime());
+
+//        //获取当前时间
+//        LocalDateTime today = LocalDateTime.now();
+//
+//        //当前时间+1天
+//        LocalDateTime tomorrow = today.plusDays(1);
+//
+//        //打印明天的时间
+//        System.out.println(tomorrow);
+
+        LocalDateTime today = LocalDateTime.now();
+
+        //minusDays()方法相当于当前时间减去n天，传负值相当于加一天
+        LocalDateTime tomorrow = today.minusDays(-1);
+
+        System.out.println(tomorrow);
+    }
+}
